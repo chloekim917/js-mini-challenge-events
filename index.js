b2cde49c2f8dbfcaf79a3727d786271912b84c13
chloekim917 <chloekim917@gmail.com>
2020-06-24 21:51:09 -0400

    Done.

--- index.js
+++ index.js
@@ -32,15 +32,41 @@ PLAYERS.forEach(renderPlayer)
 
 
 /***** Deliverable 1 *****/
-function toggleColor(element) {
-  if (element.style.color === "red") {
-    element.style.color = "black"
-  } else {
-    element.style.color = "red"
+const element = document.querySelector("h1#header")
+element.addEventListener('click', function(){
+  function toggleColor(element) {
+    if (element.style.color === "red") {
+      element.style.color = "black"
+    } else {
+      element.style.color = "red"
+    }
   }
-}
-
+  
+})
 
 /***** Deliverable 2 *****/
+const newPlayer = document.querySelector("#new-player-form")
+newPlayer.addEventListener('submit', function(e){
+  e.preventDefault()
+  const player = {
+    number: e.target.number.value,
+    name: e.target.name.value,
+    nickname: e.target.nickname.value,
+    photo: e.target.photo.value,
+    likes: 0
+  }
+  renderPlayer(player)
+  newPlayer.reset()
+})
 
 /***** Deliverable 3 *****/
+
+playerContainer.addEventListener("click", function(e){
+
+  if(e.target.className === 'like-button'){
+    const button = e.target
+    const likes = button.parentNode.querySelector(".likes")
+    likes.textContent = `${parseInt(likes.textContent)+1} likes`
+  }
+ 
+})
